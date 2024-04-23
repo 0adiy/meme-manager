66ffdad2807d67ca27205fdfa2a97da9056efdff
0adiy <adityaruva@gmail.com>
2024-04-23 16:35:53 +0530

    fix main page, change tauri config

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { invoke } from "@tauri-apps/api/tauri";
 import GridView from "./layouts/GridView.jsx";
+import { useHotkeys } from "react-hotkeys-hook";
 
 const items = [
   {
     id: 1,
     title: "The Godfather",
     year: 1972,
-    rating: 9.2,
+    rating: 1.5,
     poster:
       "https://m.media-amazon.com/images/M/MV5BM2MyNjYxNmUtYTAwNi00MTYxLWJmNWYtYzZlODY3ZTk3OTFlXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg",
   },
@@ -15,20 +16,60 @@ const items = [
     id: 2,
     title: "The Godfather: Part II",
     year: 1974,
-    rating: 9.0,
+    rating: 3.0,
     poster:
-      "https://m.media-amazon.com/images/M/MV5BMWMwMGQzZTItY2JlNC00OWZiLWIyYjMtMmZiYmEyNmU1NjMzXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg",
+      "https://www4.yts.nz/assets/images/movies/The_Godfather_Part_II_1974/large-cover.jpg",
   },
   {
     id: 3,
+    title: "The Godfather: Part III",
+    year: 1990,
+    rating: 3.5,
+    poster:
+      "https://www4.yts.nz/assets/images/movies/The_Godfather_Part_III_1990/large-cover.jpg",
+  },
+  {
+    id: 4,
     title: "The Dark Knight",
     year: 2008,
-    rating: 9.0,
+    rating: 4.5,
+    poster:
+      "https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_SX300.jpg",
+  },
+  {
+    id: 5,
+    title: "The Dark Knight Rises",
+    year: 2012,
+    rating: 4.0,
+    poster:
+      "https://m.media-amazon.com/images/M/MV5BMTk4ODQzNDY3Ml5BMl5BanBnXkFtZTcwODA0NTM4Nw@@._V1_SX300.jpg",
+  },
+  {
+    id: 6,
+    title: "Atomic Blonde",
+    year: 2021,
+    rating: 3.0,
+    poster:
+      "https://www4.yts.nz/assets/images/movies/atomic_blonde_2017/medium-cover.jpg",
+  },
+  {
+    id: 7,
+    title: "The Matrix",
+    year: 1999,
+    rating: 4.0,
+    poster:
+      "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
   },
 ];
 
 function App() {
   const [term, setTerm] = useState("");
+  const inputRef = useRef(null);
+
+  useHotkeys("ctrl+k", e => {
+    e.preventDefault();
+    inputRef.current.focus();
+  });
 
   // async function greet() {
   //   setGreetMsg(await invoke("greet", { name }));
@@ -49,7 +90,12 @@ function App() {
       >
         {/* TODO - add focus on Ctrl+K */}
         <label className='input input-bordered flex items-center gap-2'>
-          <input type='text' className='grow' placeholder='Search' />
+          <input
+            ref={inputRef}
+            type='text'
+            className='grow'
+            placeholder='Search'
+          />
           <kbd className='kbd kbd-sm'>⌘</kbd>
           <kbd className='kbd kbd-sm'>K</kbd>
         </label>
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,16 +6,31 @@ export default function Card({ movie }) {
 
   return (
     <div className='card card-compact w-full bg-base-100 shadow-xl'>
-      <figure>
+      <div className='w-full min-h-40 h-80 block overflow-hidden relative'>
         {/* suggest classes for img */}
-        <img className='w-full' src={movie?.poster} alt={movie?.title} />
-      </figure>
+        <img
+          className=' absolute inset-0 w-full h-full object-cover'
+          src={movie?.poster}
+          alt={movie?.title}
+        />
+      </div>
       <div className='card-body'>
         <h2 className='card-title'>{movie?.title}</h2>
         <p>(Description)If a dog chews shoes whose shoes does he choose?</p>
-        <div className='card-actions justify-end'>
-          <button className='btn btn-secondary'>Visit</button>
+        <div className='rating'>
+          {[1, 2, 3, 4, 5].map(num => (
+            <input
+              type='radio'
+              disabled
+              className='mask mask-star bg-yellow-500'
+              key={num}
+              checked={num == parseInt(movie?.rating)}
+            />
+          ))}
         </div>
+        {/* <div className='card-actions justify-end'>
+          <button className='btn btn-secondary'>Visit</button>
+        </div> */}
       </div>
     </div>
   );
