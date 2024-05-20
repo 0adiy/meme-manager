f2a89e1c8b0e2a78c63f57fe3a9db15f3b22d83d
0adiy <adityaruva@gmail.com>
2024-05-20 21:20:58 +0530

    fix #2 added preview and 2 col to modal on large screens

--- src/components/MemeForm.jsx
+++ src/components/MemeForm.jsx
@@ -2,6 +2,9 @@ import { useMemesStore } from "../store/useMemesStore";
 import { useMemeFormStore } from "../store/useMemeFormStore";
 import { PlusIcon } from "@heroicons/react/24/outline";
 import { open } from "@tauri-apps/api/dialog";
+import { convertFileSrc } from "@tauri-apps/api/tauri";
+import brokenImage from "../assets/brokenIMage.svg";
+import { invoke } from "@tauri-apps/api";
 
 export default function MemeForm() {
   const { insertMeme } = useMemesStore();
@@ -90,63 +93,87 @@ export default function MemeForm() {
           <h3 className='font-bold text-lg mb-4'>Meme Form</h3>
 
           <form action='' className='flex flex-col gap-2'>
-            <input
-              type='text'
-              placeholder='Name'
-              id='name'
-              onChange={e => setName(e.target.value)}
-              value={name}
-              className='input input-bordered w-full text-base'
-            />
-            <input
-              type='text'
-              id='url'
-              placeholder='Url'
-              onChange={e => setUrl(e.target.value)}
-              value={url}
-              className='input input-bordered w-full text-base'
-            />
-            <input
-              type='button'
-              className='btn btn-neutral w-full'
-              onClick={handelFileOpen}
-              value={"Select File"}
-            />
-            <p className='mb-4 bold'>
-              Local Path: <span className='text-accent'>{localPath}</span>
-            </p>
-
-            <label className='label cursor-pointer m-auto w-fit gap-4'>
-              <span className='label-text'>Image</span>
-              <input
-                type='checkbox'
-                className='toggle'
-                onChange={e =>
-                  filetype === "image"
-                    ? setFiletype("video")
-                    : setFiletype("image")
-                }
-                checked={filetype === "video"}
-              />
-              <span className='label-text'>Video</span>
-            </label>
-
-            <textarea
-              className='textarea textarea-bordered textarea-xs w-full text-base px-3'
-              placeholder='Description'
-              onChange={e => setDescription(e.target.value)}
-              value={description}
-              id='description'
-              rows='3'
-            />
-            <textarea
-              className='textarea textarea-bordered textarea-xs w-full text-base px-3'
-              placeholder='Tags (Comma separated)'
-              onChange={e => setTags(e.target.value)}
-              value={tags}
-              id='tags'
-              rows='2'
-            />
+            <div className='flex flex-col w-full lg:flex-row'>
+              {/* left side */}
+              <div className='flex flex-col flex-grow gap-2'>
+                <input
+                  type='text'
+                  placeholder='Name'
+                  id='name'
+                  onChange={e => setName(e.target.value)}
+                  value={name}
+                  className='input input-bordered w-full text-base'
+                />
+                <input
+                  type='text'
+                  id='url'
+                  placeholder='Url'
+                  onChange={e => setUrl(e.target.value)}
+                  value={url}
+                  className='input input-bordered w-full text-base'
+                />
+                <textarea
+                  className='textarea textarea-bordered textarea-xs w-full text-base px-3'
+                  placeholder='Tags (Comma separated)'
+                  onChange={e => setTags(e.target.value)}
+                  value={tags}
+                  id='tags'
+                  rows='2'
+                />{" "}
+                <textarea
+                  className='textarea textarea-bordered textarea-xs w-full text-base px-3'
+                  placeholder='Description'
+                  onChange={e => setDescription(e.target.value)}
+                  value={description}
+                  id='description'
+                  rows='3'
+                />
+              </div>
+              {/* divider */}
+              <div className='divider lg:divider-horizontal'></div>
+              {/* right side */}
+              <div className='flex flex-col lg:w-40 gap-2'>
+                <img
+                  className='w-full bg-base-300 rounded-btn flex-grow object-cover'
+                  src={convertFileSrc(localPath)}
+                  onError={e => {
+                    e.target.src = brokenImage;
+                  }}
+                  alt='preview'
+                />
+                <input
+                  type='button'
+                  className='btn btn-neutral w-full'
+                  onClick={handelFileOpen}
+                  value={"Select File"}
+                />
+                <label className='label cursor-pointer mx-auto w-fit gap-2'>
+                  <span className='label-text'>Image</span>
+                  <input
+                    type='checkbox'
+                    className='toggle'
+                    onChange={e =>
+                      filetype === "image"
+                        ? setFiletype("video")
+                        : setFiletype("image")
+                    }
+                    checked={filetype === "video"}
+                  />
+                  <span className='label-text'>Video</span>
+                </label>
+              </div>
+            </div>
+            <p>Local Path:</p>
+            <a
+              className='link link-accent font-mono break-all line-clamp-1'
+              onClick={e => {
+                e.stopPropagation();
+                e.preventDefault();
+                invoke("show_in_folder", { path: localPath });
+              }}
+            >
+              {localPath}
+            </a>
             <div className='modal-action flex'>
               <button className='btn btn-primary' onClick={handleOnSubmit}>
                 Submit
